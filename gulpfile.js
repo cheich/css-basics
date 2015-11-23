309e30513a5441c640b532e6d1de4ed92a7724ce
Christoph Heich <info@christoph-heich.de>
2015-11-23 01:45:26 +0100

    Bugfixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,90 @@
 /*!
- * CSS Basics gulpfile v1.0.0
+ * CSS Basics gulpfile v1.1.0
  */
  
 'use strict';
 
 var gulp = require('gulp');
+var rename = require('gulp-rename');
+var header = require('gulp-header');
 
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify-css');
 var sourcemaps = require('gulp-sourcemaps');
 
-var rename = require('gulp-rename');
-var header = require('gulp-header');
-
-var date = new Date();
 var pkg = require('./package.json');
+pkg.year = String(new Date().getFullYear());
 
-/**
- * Banners
- */
- 
+var browserlist = ['> 1%', 'last 2 versions', 'Firefox ESR'];
+var sassIncludePaths = 'scss/basics/components';
+
 var banner = '/*!\n' + 
-             ' * CSS Basics v<%= pkg.version %> <<%= pkg.homepage %>\n' + 
-             ' * Copyright 2014-' + date.getFullYear() + ' <%= pkg.author.name %> <<%= pkg.author.url %>>\n' + 
+             ' * <%= pkg.title %> v<%= pkg.version %> <<%= pkg.homepage %>>\n' + 
+             ' * Copyright 2014-<%= pkg.year %> <%= pkg.author.name %> <<%= pkg.author.url %>>\n' + 
              ' * Released under the <%= pkg.license %> license <https://github.com/cheich/CSS-Basics/blob/master/LICENSE>\n' + 
              ' */\n';
 
-var customBanner = '/*!\n' + 
-                   ' * CSS Basics Custom v<%= pkg.version %> <<%= pkg.homepage %>\n' + 
-                   ' * Copyright 2014-' + date.getFullYear() + ' <%= pkg.author.name %> <<%= pkg.author.url %>>\n' + 
-                   ' * Released under the <%= pkg.license %> license <https://github.com/cheich/CSS-Basics/blob/master/LICENSE>\n' + 
-                   ' */\n';
-
 var themeBanner = '/*!\n' + 
-                  ' * Theme - build on CSS Basics v<%= pkg.version %>\n' + 
+                  ' * Theme - build on CSS Basics v<%= pkg.version %>>\n' + 
                   ' */';
-
-/**
- * Compile all files from gulpSrc
- *
- * Includes prefixing, minifying and source-maps.
- * Will NOT generate any files.
- * 
- * @param gulpSrc Files to compile
- * @return gulp
- */
-var compile = function(gulpSrc) {
-    return gulpSrc
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: [
-                'scss/basics/components',
-            ],
-        }).on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(minify());
-};
 
 /**
  * Default task
  */
 gulp.task('default', function() {
-    compile(gulp.src('scss/basics.scss'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('dist'));
-    compile(gulp.src('scss/theme.scss'))
-        .pipe(sourcemaps.write('./'))
-        .pipe(header(themeBanner, { pkg: pkg }))
-        .pipe(gulp.dest('dist'));
+    gulp.start('basics');
+    gulp.start('theme');
 });
 
 /**
- * Watching
+ * Compile CSS Basics framework
+ *
+ * Options:
+ *   --custom: Safe as custom build. Optional with keyword.
+ */
+gulp.task('basics', function() {
+    var renameOptions = {};
+
+    // Check for custom build
+    var customIndex = process.argv.indexOf('--custom');
+    if (customIndex > -1) {
+        renameOptions.dirname = 'custom';
+        if ((customIndex + 1) in process.argv) {
+            renameOptions.suffix = '-' + process.argv[customIndex + 1].toLowerCase();
+        }else{
+            renameOptions.suffix = '-custom';
+        }
+    }
+    
+    return gulp.src('scss/basics.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(browserlist))
+        .pipe(minify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename(renameOptions))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Compile Theme
+ */
+gulp.task('theme', function() {
+    return gulp.src('scss/theme.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(browserlist))   
+        .pipe(minify())
+        .pipe(header(themeBanner, { pkg: pkg }))    
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./dist'));
+});
+
+/**
+ * Watch all changes
  */
 gulp.task('watch', function() {
     gulp.watch('scss/*.scss', ['default']);
-});
-
-
-/**
- * Custom dist
- * 
- * Add keywords via `--keyword keywordGoesHere`
- */
-gulp.task('custom', function() {
-    // Get keyword
-    var keyword = process.argv.indexOf('--keyword');    
-    if (keyword > -1)
-        keyword = process.argv[keyword + 1];    
-    if (keyword == -1 || typeof keyword === 'undefined')
-        keyword = 'custom';
-
-    // Compile
-    compile(gulp.src('scss/basics.scss'))
-        .pipe(rename({
-            suffix: '-' + keyword.toLowerCase(),
-            dirname: 'custom',
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(header(customBanner, { pkg: pkg }))
-        .pipe(gulp.dest('dist'));
 });
