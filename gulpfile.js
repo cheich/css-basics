179eb4b124ecc8a13a30816b2c07870991839789
Christoph Heich <info@christoph-heich.de>
2016-06-09 02:22:25 +0200

    New custom dist handler
    Custom dists now compiled via basics-*.scss + 'custom' task

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
-/*!
- * CSS Basics gulpfile v1.1.0
- */
-
-/* jshint node: true */
+//
+// CSS Basics gulpfile
+//
 
 'use strict';
 
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var header = require('gulp-header');
-
+var ignore = require('gulp-ignore');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var minify = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var minify = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
+var flexibility = require('postcss-flexibility');
 
 var pkg = require('./package.json');
 pkg.year = String(new Date().getFullYear());
 
 var browserlist = ['> 1%', 'last 2 versions', 'Firefox ESR'];
-var sassIncludePaths = 'scss/basics/components';
+
+var sassOptions = {
+  outputStyle: 'expanded',
+  sourceComments: false
+};
 
 var banner = '/*!\n' +
              ' * <%= pkg.title %> v<%= pkg.version %> <<%= pkg.homepage %>>\n' +
@@ -28,46 +32,63 @@ var banner = '/*!\n' +
              ' */\n';
 
 var themeBanner = '/*!\n' +
-                  ' * Theme - build on CSS Basics v<%= pkg.version %>>\n' +
-                  ' */';
+                  ' * Theme build on CSS Basics v<%= pkg.version %>\n' +
+                  ' */\n';
 
-// 
+//
 // Default task
-// 
-gulp.task('default', function() {
-  gulp.start('basics');
-  gulp.start('theme');
-});
+//
+gulp.task('default', ['basics', 'theme', 'custom']);
 
 //
 // Compile CSS Basics framework
 //
-// Options:
-//   --custom: Safe as custom build. Optional with keyword.
-//
 gulp.task('basics', function() {
-  var renameOptions = {};
-
-  // Check for custom build
-  var customIndex = process.argv.indexOf('--custom');
-  if (customIndex > -1) {
-    renameOptions.dirname = 'custom';
-    if ((customIndex + 1) in process.argv) {
-      renameOptions.suffix = '-' + process.argv[customIndex + 1].toLowerCase();
-    }else{
-      renameOptions.suffix = '-custom';
-    }
-  }
-
   return gulp.src('scss/basics.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(browserlist))
-    .pipe(minify())
+    .pipe(sass(sassOptions).on('error', function(error) {
+      throw(error.message);
+    }))
+    .pipe(postcss([
+      flexibility,
+      autoprefixer({ browsers: browserlist })
+    ]))
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(rename(renameOptions))
     .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(ignore.exclude('*.map'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minify())
     .pipe(gulp.dest('./dist'));
+});
+
+//
+// Compile a custom build of the CSS Basics framework
+//
+gulp.task('custom', function() {
+  var cpkg = pkg;
+  cpkg.version = pkg.version + ' CUSTOM BUILD';
+
+  return gulp.src('scss/basics-*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', function(error) {
+      throw(error.message);
+    }))
+    .pipe(postcss([
+      flexibility,
+      autoprefixer({ browsers: browserlist })
+    ]))
+    .pipe(header(banner, { pkg: cpkg }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist/custom'))
+    .pipe(ignore.exclude('*.map'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minify())
+    .pipe(gulp.dest('./dist/custom'));
 });
 
 //
@@ -76,11 +97,21 @@ gulp.task('basics', function() {
 gulp.task('theme', function() {
   return gulp.src('scss/theme.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer(browserlist))
-    .pipe(minify())
+    .pipe(sass(sassOptions).on('error', function(error) {
+      throw(error.message);
+    }))
+    .pipe(postcss([
+      flexibility,
+      autoprefixer({ browsers: browserlist })
+    ]))
     .pipe(header(themeBanner, { pkg: pkg }))
     .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(ignore.exclude('*.map'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(minify())
     .pipe(gulp.dest('./dist'));
 });
 
@@ -88,6 +119,7 @@ gulp.task('theme', function() {
 // Watch all changes
 //
 gulp.task('watch', function() {
-  gulp.watch('scss/*.scss', ['default']);
+  gulp.watch('scss/basics-*.scss', ['custom']);
+  gulp.watch(['scss/basics.scss', 'scss/basics/**/*.scss'], ['basics']);
   gulp.watch('scss/theme.scss', ['theme']);
 });
