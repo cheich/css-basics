7118e1f285a5efbbecb1fb31da802609dae7b9ce
Christoph Heich <info@christoph-heich.de>
2016-07-21 22:15:39 +0200

    Remove doc stuff

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,6 @@ var minify = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
 var flexibility = require('postcss-flexibility'); // testing
-var sassdoc = require('sassdoc');
-var cp = require('child_process'); // native
 
 var pkg = require('./package.json');
 pkg.year = String(new Date().getFullYear());
@@ -31,12 +29,8 @@ var sassOptions = {
 var banner = '/*!\n' +
              ' * <%= pkg.title %> v<%= pkg.version %> <<%= pkg.homepage %>>\n' +
              ' * Copyright 2014-<%= pkg.year %> <%= pkg.author.name %> <<%= pkg.author.url %>>\n' +
-             ' * Released under the <%= pkg.license %> license <https://github.com/cheich/CSS-Basics/blob/master/LICENSE>\n' +
+             ' * Released under the <%= pkg.license %> license <https://github.com/cheich/css-basics/blob/master/LICENSE>\n' +
              ' */\n';
-
-var themeBanner = '/*!\n' +
-                  ' * Theme build on CSS Basics v<%= pkg.version %>\n' +
-                  ' */\n';
 
 //
 // Default task
@@ -86,7 +80,7 @@ gulp.task('custom', function() {
       flexibility,
       autoprefixer({ browsers: browserlist })
     ]))
-    .pipe(header(banner, { pkg: pkg }));
+    .pipe(header(banner, { pkg: cpkg }));
 
   var main = stream.pipe(clone())
     .pipe(sourcemaps.write('./'))
@@ -101,62 +95,9 @@ gulp.task('custom', function() {
 });
 
 //
-// Compile documentation theme
-//
-gulp.task('doc-style', function() {
-  var stream = gulp.src('docs/assets/_scss/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass(sassOptions).on('error', function(error) {
-      throw(error.message);
-    }))
-    .pipe(postcss([
-      flexibility,
-      autoprefixer({ browsers: browserlist })
-    ]))
-    .pipe(header(banner, { pkg: pkg }));
-
-  var main = stream.pipe(clone())
-    .pipe(sourcemaps.write('./', {
-      sourceRoot: './'
-    }))
-    .pipe(gulp.dest('./docs/assets/css'));
-
-  var min = stream.pipe(clone())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(minify())
-    .pipe(gulp.dest('./docs/assets/css'));
-
-  return merge(main, min);
-});
-
-//
-// Sass documentation
-//
-gulp.task('sassdoc', function() {
-  return gulp.src('./scss/basics/**/*.scss')
-    .pipe(sassdoc({
-      dest: 'docs/_site/sassdoc'
-    }));
-});
-
-//
-// Build HTML documentation
-//
-gulp.task('doc', ['doc-style'], function() {
-  return cp.spawn('jekyll.bat', ['build'], {
-    stdio: 'inherit',
-    cwd: 'docs'
-  });
-});
-
-//
 // Watch all changes
 //
 gulp.task('watch', function() {
   gulp.watch('scss/basics-*.scss', ['custom']);
   gulp.watch(['scss/basics.scss', 'scss/basics/**/*.scss'], ['basics']);
-  cp.spawn('jekyll.bat', ['build', '--watch'], {
-    stdio: 'inherit',
-    cwd: 'docs'
-  });
 });
