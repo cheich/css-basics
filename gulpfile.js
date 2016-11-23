b489adb0c95498a6c389f85311e018acae7b1ec4
Christoph Heich <mail@christophheich.de>
2016-11-23 21:30:36 +0100

    Added documentation

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,12 @@ var postcss = require('gulp-postcss');
 var minify = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('autoprefixer');
+var sassdoc = require('sassdoc');
+var cp = require('child_process');
+var fs = require('fs-extra');
 var flexibility = require('postcss-flexibility'); // testing
 
-var pkg = require('./package.json');
-pkg.year = String(new Date().getFullYear());
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 
 var browserlist = ['> 1%', 'last 2 versions', 'Firefox ESR'];
 
@@ -26,22 +28,27 @@ var sassOptions = {
   sourceComments: false
 };
 
+var pkg = require('./package.json');
+pkg.year = String(new Date().getFullYear());
+
 var banner = '/*!\n' +
              ' * <%= pkg.title %> v<%= pkg.version %> <<%= pkg.homepage %>>\n' +
              ' * Copyright 2014-<%= pkg.year %> <%= pkg.author.name %> <<%= pkg.author.url %>>\n' +
              ' * Released under the <%= pkg.license %> license <https://github.com/cheich/css-basics/blob/master/LICENSE>\n' +
              ' */\n';
 
-var path = {
-  custom: ['scss/basics-*.scss', '!scss/basics-rtl.scss'],
-  default: ['scss/basics.scss', 'scss/basics-rtl.scss']
+var srcPath = {
+  custom:  ['scss/basics-*.scss', '!scss/basics-rtl.scss'],
+  default: ['scss/basics.scss', 'scss/basics-rtl.scss'],
+  sassdoc: './scss/basics/**/*.scss',
+  docs:    './docs'
 };
 
 //
-// Compile CSS Basics framework
+// Compile the CSS Basics framework
 //
 gulp.task('default', function() {
-  var stream = gulp.src(path.default)
+  var stream = gulp.src(srcPath.default)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', function(error) {
       throw(error.message);
@@ -71,7 +78,7 @@ gulp.task('custom', function() {
   var cpkg = pkg;
   cpkg.version = pkg.version + ' CUSTOM BUILD';
 
-  var stream = gulp.src(path.custom)
+  var stream = gulp.src(srcPath.custom)
     .pipe(sourcemaps.init())
     .pipe(sass(sassOptions).on('error', function(error) {
       throw(error.message);
@@ -95,8 +102,37 @@ gulp.task('custom', function() {
 });
 
 //
+// Build documentation
+//
+gulp.task('docs', ['sassdoc'], function (done) {
+  fs.copy('./scss/basics', './docs/_sass/.basics-tmp', function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+
+  return cp.spawn(jekyll, ['build'], {
+      stdio: 'inherit',
+      cwd: srcPath.docs
+    })
+    .on('close', done);
+});
+
+//
+// Sass documentation
+//
+gulp.task('sassdoc', function() {
+  return gulp.src(srcPath.sassdoc)
+    .pipe(sassdoc({
+      dest: 'docs/sassdoc'
+    }));
+});
+
+//
 // Watch all changes
 //
-gulp.task('watch', function() {
-  gulp.watch(['scss/**/*.scss'], ['basics', 'custom']);
+gulp.task('watch', function () {
+  gulp.watch(['scss/**/*.scss'], ['default', 'custom']);
+  gulp.watch(['**/*.html', '**/*.md', '!README.md', '_sass/_theme.scss', 'assets/**/*'], ['doc']);
+  gulp.watch(['_sass/basics/**/*.scss'], ['sassdoc']);
 });
